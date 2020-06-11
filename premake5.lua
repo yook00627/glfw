afaa993c141aa6fcf2ab55fbca7afad5a78365ff
Kevin Yook <yook00627@gmail.com>
2020-06-10 17:58:42 -0700

    Add missing files time.c

--- premake5.lua
+++ premake5.lua
@@ -27,6 +27,7 @@ project "GLFW"
             "src/win32_init.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
+            "src/win32_time.c",
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
